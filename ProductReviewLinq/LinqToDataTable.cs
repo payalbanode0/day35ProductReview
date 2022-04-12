e7b72af2006d3748836533213172d8c38e42edb5
payalbanode0 <payalbanode0@gmail.com>
2022-04-12 20:59:36 +0530

    average raating

--- ProductReviewLinq/LinqToDataTable.cs
+++ ProductReviewLinq/LinqToDataTable.cs
@@ -15,42 +15,47 @@ namespace productReviewLinq
             DataTable dt = new DataTable();
             dt.Columns.Add("ID");
             dt.Columns.Add("ProductName");
+            dt.Columns.Add("ProductID");
             dt.Columns.Add("Review");
             dt.Columns.Add("Rating");
             dt.Columns.Add("IsLike");
 
 
-            dt.Rows.Add("1", "Chai", "Good", 8, true);
-            dt.Rows.Add("2", "Coffee", "Good", 6, true);
-            dt.Rows.Add("3", "Green Tea", "Bad", 4, false);
-            dt.Rows.Add("4", "Thumbs UP", "Great", 9, true);
-            dt.Rows.Add("5", "Montian Dew", "Good", 9, true);
-            dt.Rows.Add("6", "Sting", "Bad", 4, false);
-            dt.Rows.Add("7", "Maaza", "Very Good", 10, true);
-            dt.Rows.Add("8", "Frooti", "Good", 7, true);
-            dt.Rows.Add("9", "Sprit", "Bad", 3, false);
-            dt.Rows.Add("10", "coco cola", "Great", 10, true);
-            dt.Rows.Add("11", "Limca", "Good", 9, true);
-            dt.Rows.Add("12", "Bisleri", "Good", 6, true);
-            dt.Rows.Add("13", "Slice", "Good", 8, true);
-            dt.Rows.Add("14", "Kinley", "Good", 6, true);
-            dt.Rows.Add("15", "Kafe Strong", "Good", 7, true);
-            dt.Rows.Add("16", "Knock Out", "Great", 9, true);
-            dt.Rows.Add("17", "Kafe Lite", "Worst", 2, false);
-            dt.Rows.Add("18", "Rum", "Great", 10, true);
-            dt.Rows.Add("19", "Whiskey", "Very Good", 10, true);
-            dt.Rows.Add("20", "Vodka", "Good", 7, true);
-            dt.Rows.Add("21", "Brandi", "Bad", 3, false);
-            dt.Rows.Add("22", "Champaine", "Great", 10, true);
-            dt.Rows.Add("23", "RedBull", "Good", 9, true);
-            dt.Rows.Add("24", "Gluco plus", "Good", 8, true);
-            dt.Rows.Add("25", "Red Wine", "Okay", 6, true);
+            dt.Rows.Add(1, "Chai", 1, "Good", 8, true);
+            dt.Rows.Add(2, "Coffee", 1, "nice", 6, true);
+            dt.Rows.Add(3, "Green Tea", 1, "Bad", 4, false);
+            dt.Rows.Add(4, "Thumbs UP", 2, "Great", 9, true);
+            dt.Rows.Add(5, "Montian Dew", 2, "Good", 9, true);
+            dt.Rows.Add(6, "Sting", 2, "Bad", 4, false);
+            dt.Rows.Add(7, "Maaza", 3, "Very Good", 10, true);
+            dt.Rows.Add(8, "Frooti", 3, "Good", 7, true);
+            dt.Rows.Add(9, "Sprit", 2, "Bad", 3, false);
+            dt.Rows.Add(10, "coco cola", 2, "Great", 10, true);
+            dt.Rows.Add(11, "Limca", 3, "Good", 9, true);
+            dt.Rows.Add(12, "Bisleri", 4, "Good", 6, true);
+            dt.Rows.Add(13, "Slice", 3, "Good", 8, true);
+            dt.Rows.Add(14, "Kinley", 4, "Good", 6, true);
+            dt.Rows.Add(15, "Kafe Strong", 5, "nice", 7, true);
+            dt.Rows.Add(16, "Knock Out", 5, "Great", 9, true);
+            dt.Rows.Add(17, "Kafe Lite", 5, "Worst", 2, false);
+            dt.Rows.Add(18, "Rum", 10, "Great", 10, true);
+            dt.Rows.Add(19, "Whiskey", 10, "Very Good", 10, true);
+            dt.Rows.Add(20, "Vodka", 10, "nice", 7, true);
+            dt.Rows.Add(21, "Brandi", 10, "Bad", 3, false);
+            dt.Rows.Add(22, "Champaine", 5, "Great", 10, true);
+            dt.Rows.Add(23, "RedBull", 2, "nice", 9, true);
+            dt.Rows.Add(24, "Gluco plus", 2, "Good", 8, true);
+            dt.Rows.Add(25, "Red Wine", 10, "Okay", 6, true);
 
             //    DisplayProducts(dt);
-            Retrieve_ISLike(dt);
+            //Uc 9
+            //Retrieve_ISLike(dt);
 
+            //Uc 10
+            Retrieve_Avg_Prodructs(dt);
 
-
+            //UC 11
+            //NiceReview(dt);
         }
         public void DisplayProducts(DataTable dt)
         {
@@ -58,9 +63,10 @@ namespace productReviewLinq
             {
                 string Id = row["ID"].ToString();
                 string Name = row["ProductName"].ToString();
+                int ProductIf = Convert.ToInt32(row["ProductID"].ToString());
                 string Review = row["Review"].ToString();
                 string rating = row["Rating"].ToString();
-                string ISLike = row["IsLike"].ToString();
+                bool ISLike = Convert.ToBoolean(row["IsLike"].ToString());
                 Console.WriteLine("ID :" + Id + ", ProductName : " + Name + ", Review : " + Review + ", Rating : " + rating + ", IsLike : " + ISLike);
 
             }
@@ -78,6 +84,38 @@ namespace productReviewLinq
                 }
             }
 
+        }
+        //UC 10
+        public void Retrieve_Avg_Prodructs(DataTable dt)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                double avg1 = dt.Select().Where(p => Convert.ToInt32(p["ProductId"]) == 1).Select(c => Convert.ToDouble(c["Rating"])).Average();
+                double avg2 = dt.Select().Where(p => Convert.ToInt32(p["ProductId"]) == 2).Select(c => Convert.ToDouble(c["Rating"])).Average();
+                double avg3 = dt.Select().Where(p => Convert.ToInt32(p["ProductId"]) == 3).Select(c => Convert.ToDouble(c["Rating"])).Average();
+                double avg4 = dt.Select().Where(p => Convert.ToInt32(p["ProductId"]) == 4).Select(c => Convert.ToDouble(c["Rating"])).Average();
+                double avg5 = dt.Select().Where(p => Convert.ToInt32(p["ProductId"]) == 5).Select(c => Convert.ToDouble(c["Rating"])).Average();
+                double avg10 = dt.Select().Where(p => Convert.ToInt32(p["ProductId"]) == 10).Select(c => Convert.ToDouble(c["Rating"])).Average();
+                Print(avg1, avg2, avg3, avg4, avg5, avg10);
+                void Print(double avg1, double avg2, double avg3, double avg4, double avg5, double avg10)
+                {
+                    Console.WriteLine("Product Id : 1, Avg2 : " + avg1 + "\nProduct Id : 2, Avg : " + avg2 + "\nProduct Id : 3, Avg : " + avg3 + "\nProduct Id : 4, Avg : " + avg4 + "\nProduct Id : 1, Avg : " + avg5 + "\nProduct Id : 10, Avg : " + avg10);
+                }
+                break;
+            }
+        }
+        //UC 11
+        public void NiceReview(DataTable dt)
+        {
+            foreach (DataRow row in dt.Rows)
+            {
+                string IsLike = row["REview"].ToString();
+                string Name = row["ProductName"].ToString();
+                if (IsLike == "nice")
+                {
+                    Console.WriteLine("Prduct Name : " + Name + ", Review : " + IsLike);
+                }
+            }
         }
 
 
--- ProductReviewLinq/Program.cs
+++ ProductReviewLinq/Program.cs
@@ -8,38 +8,36 @@ namespace productReviewLinq
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("LINQ to Data Table!");
-            //LinqToDataTable ldt = new LinqToDataTable();
-            //ldt.AddToTable();
+
 
             //UC 1
             Console.WriteLine("Product Review Management with Linq");
-            List<ProductReview> listofpr = new List<ProductReview>()
-            {
-                new ProductReview(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true},
-                new ProductReview(){ProductID=1,UserID=2,Rating=4,Review="Okay",isLike=false},
-                new ProductReview(){ProductID=2,UserID=3,Rating=6,Review="Good",isLike=true},
-                new ProductReview(){ProductID=2,UserID=4,Rating=3,Review="Bad",isLike=false},
-                new ProductReview(){ProductID=3,UserID=5,Rating=8,Review="Good",isLike=true},
-                new ProductReview(){ProductID=4,UserID=6,Rating=2,Review="Very Bad",isLike=false},
-                new ProductReview(){ProductID=5,UserID=7,Rating=10,Review="Perfect",isLike=true},
-                new ProductReview(){ProductID=5,UserID=8,Rating=7,Review="Nice",isLike=false},
-                new ProductReview(){ProductID=3,UserID=9,Rating=0,Review="Very Bad",isLike=false},
-                new ProductReview(){ProductID=4,UserID=10,Rating=4,Review="Okay",isLike=false},
-                new ProductReview(){ProductID=5,UserID=11,Rating=6.5,Review="Good",isLike=true},
-                new ProductReview(){ProductID=2,UserID=12,Rating=3,Review="Bad",isLike=false},
-                new ProductReview(){ProductID=3,UserID=13,Rating=8,Review="Very Good",isLike=true},
-                new ProductReview(){ProductID=9,UserID=14,Rating=2,Review="Bad",isLike=false},
-                new ProductReview(){ProductID=9,UserID=15,Rating=9,Review="Perfect",isLike=true},
-                new ProductReview(){ProductID=9,UserID=16,Rating=7,Review="Nice",isLike=false}
-            };
+            //List<ProductReview> listofpr = new List<ProductReview>()
+            //{
+            //    new ProductReview(){ProductID=1,UserID=1,Rating=5,Review="Good",isLike=true},
+            //    new ProductReview(){ProductID=1,UserID=2,Rating=4,Review="Okay",isLike=false},
+            //    new ProductReview(){ProductID=2,UserID=3,Rating=6,Review="Good",isLike=true},
+            //    new ProductReview(){ProductID=2,UserID=4,Rating=3,Review="Bad",isLike=false},
+            //    new ProductReview(){ProductID=3,UserID=5,Rating=8,Review="Good",isLike=true},
+            //    new ProductReview(){ProductID=4,UserID=6,Rating=2,Review="Very Bad",isLike=false},
+            //    new ProductReview(){ProductID=5,UserID=7,Rating=10,Review="Perfect",isLike=true},
+            //    new ProductReview(){ProductID=5,UserID=8,Rating=7,Review="Nice",isLike=false},
+            //    new ProductReview(){ProductID=3,UserID=9,Rating=0,Review="Very Bad",isLike=false},
+            //    new ProductReview(){ProductID=4,UserID=10,Rating=4,Review="Okay",isLike=false},
+            //    new ProductReview(){ProductID=5,UserID=11,Rating=6.5,Review="Good",isLike=true},
+            //    new ProductReview(){ProductID=2,UserID=12,Rating=3,Review="Bad",isLike=false},
+            //    new ProductReview(){ProductID=3,UserID=13,Rating=8,Review="Very Good",isLike=true},
+            //    new ProductReview(){ProductID=9,UserID=14,Rating=2,Review="Bad",isLike=false},
+            //    new ProductReview(){ProductID=9,UserID=15,Rating=9,Review="Perfect",isLike=true},
+            //    new ProductReview(){ProductID=9,UserID=16,Rating=7,Review="Nice",isLike=false}
+            //};
             //foreach (var list in listofpr)
             //{
             //    Console.WriteLine("ProductID : " + list.ProductID + ", User ID : " + list.UserID + ", Rating :" + list.Rating + ", Review : " + list.Review + ", isLike : " + list.isLike);
             //}
 
             //UC 2
-            Management management = new Management();
+            //Management management = new Management();
             //management.TopRecords(listofpr);
 
             //UC 3
@@ -56,10 +54,16 @@ namespace productReviewLinq
 
             //UC 7
             //management.RetrieveProductId_RatingUsingSelect(listofpr);
-            
+
             //UC 8
-            Console.WriteLine("LINQ to Data Table!");
+            //Console.WriteLine("LINQ to Data Table!");
             LinqToDataTable ldt = new LinqToDataTable();
+            //ldt.AddToTable();
+
+            //UC 9
+            //ldt.AddToTable();
+
+            //UC 10
             ldt.AddToTable();
 
         }
